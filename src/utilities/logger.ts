0d202d7f5cf5a7f31ddcd2d214fc12ea2304a357
mahmoudv2017 <mahmoud.elbermbaly2017@yahoo.com>
2022-12-19 22:09:36 +0200

    third

--- src/utilities/logger.ts
+++ src/utilities/logger.ts
@@ -9,7 +9,7 @@ async function logger(req:express.Request,res:express.Response,next:Function){
     if (!req.query.width || !(req.query.height) || !req.query.filename) { res.status(304).send("few params"); }
     else{
         if( (req.query.width == "0" || req.query.width == "-1") || (req.query.height == "0" || req.query.height == "-1")){
-            res.status(304).send("few params");
+            res.status(304).send("Wrong Params");
         }else{
             next();
         }
